6a12af24d7e7306b49db2bdb214c40cbdf526699
weizhou@apache.org <Wei Zhou>
2023-03-22 13:39:06 +0000

    Ubuntu22: install python3-pip

--- deploy_tungsten_centos7.sh
+++ deploy_tungsten_centos7.sh
@@ -16,24 +16,25 @@ elif [ -f /etc/os-release ] ; then
   CODENAME=`grep -e "^PRETTY_NAME=" /etc/os-release | awk -F\" '{print $2}'`
 fi
 
-if [[ ! $REV =~ 8.* ]] && [[ ! $REV =~ 7.* ]];then
+if [[ ! $REV =~ ^8.* ]] && [[ ! $REV =~ ^7.* ]];then
   echo "This script can ONLY be executed on CentOS/Rocky 7/8"
   exit 1
 fi
 
 # 2. Install packages
 
-if [[ ! $REV =~ 7.* ]];then
+if [[ $REV =~ ^7.* ]];then
   yum install -y epel-release sshpass
-  yum install -y python2-pip
+  rpm -qa |grep python2-pip || yum install -y python2-pip
 
   pip install pip==20.3.4
   pip install requests
   pip install --ignore-installed PyYAML
   pip install ansible==2.7.18
-elif [[ ! $REV =~ 8.* ]];then
-  yum install -y python3-pip
+elif [[ $REV =~ ^8.* ]];then
+  rpm -qa |grep python3-pip || yum install -y python3-pip
   pip3 install requests
+  pip3 install --upgrade PyYAML
   ls /usr/bin/python || ln -s /usr/bin/python3 /usr/bin/python
 fi
 
